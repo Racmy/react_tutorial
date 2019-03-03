f5495b6658408c36324fe6cc014113ee2717e1c3
yutoyoden <yutoyoden0204@gmail.com>
2019-03-03 20:29:55 +0900

    add lintrc and game.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={count:0};
+    this.state = { count: 0 };
   }
 
-  handleClick(){
-    this.setState({count:this.state.count + 1});
+  handleClick() {
+    this.setState({ count: this.state.count + 1 });
   }
   render() {
     return (
       <div>
-        <h1>
-          {this.state.count}
-        </h1>
-        <button onClick={()=>{this.handleClick()}}>+</button>
-        <input type="text"></input>
+        <h1>{this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.handleClick();
+          }}
+        >
+          +
+        </button>
       </div>
-      
-      
     );
   }
 }

12ca3ef4fd40b3cc392b2418967828a137b67c41
yutoyoden <yutoyoden0204@gmail.com>
2019-03-03 20:46:17 +0900

    index.html id 修正and webpack lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
-const  HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode:'development',
-    // エントリポイントのjsxファイル
-    entry: './src/main.js',
-        
-    output: {
-        filename: 'bundle.js',
-        //path: __dirname + '/dist'
-    },
-    module: {
-        rules: [{
-            test: /\.js?$/, // 拡張子がjsで
-            exclude: /node_modules/, // node_modulesフォルダ配下は除外
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [ '@babel/preset-env','@babel/react']
-                }
-                
-            }
-        },{
-            test: /\.html$/,
-            use: [
-                {
-                    loader: "html-loader"
-                }
-            ]
+  mode: "development",
+  // エントリポイントのjsxファイル
+  entry: "./src/main.js",
+
+  output: {
+    filename: "bundle.js"
+    //path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/, // 拡張子がjsで
+        exclude: /node_modules/, // node_modulesフォルダ配下は除外
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/react"]
+          }
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
         ]
-        },
-        plugins:[
-            new HtmlWebPackPlugin({
-                template:"./src/index.html",
-                filename: "./index.html"
-            })
-        ]
-  };
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};

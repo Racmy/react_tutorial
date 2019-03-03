0bb20fc55600e111ff0821b91cd6eee6bf89fa56
yutoyoden <yutoyoden0204@gmail.com>
2019-03-03 21:34:53 +0900

    pretter 忘れてたので追加

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
+    constructor(props) {
+        super(props)
+        this.state = { count: 0 }
+    }
 
-  handleClick() {
-    this.setState({ count: this.state.count + 1 });
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button
-          onClick={() => {
-            this.handleClick();
-          }}
-        >
-          +
-        </button>
-      </div>
-    );
-  }
+    handleClick() {
+        this.setState({ count: this.state.count + 1 })
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button
+                    onClick={() => {
+                        this.handleClick()
+                    }}
+                >
+                    +
+                </button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import Game from "./game";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Game from './game'
 
-ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<Game />, document.getElementById("container"));
+ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Game />, document.getElementById('container'))
